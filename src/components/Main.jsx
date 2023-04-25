e255677c28ac3cb67c7793dc05ee4cc3b6560734
edel-blk <stanminesilver@gmail.com>
2023-04-25 14:50:05 -0700

    Anexo de appBar a aplicacion

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Constants from 'expo-constants';
 import { View, Text } from 'react-native';
 import RepositoryList from './RepositoryList.jsx';
+import AppBar from './AppBar.jsx';
 
 export default function main() {
   return (
-    <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
+    <View style={{ flexGrow: 1 }}>
+      <AppBar/>
       <RepositoryList/>
     </View>
   )
--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,25 @@
 const theme = {
-    colors: {
-        textPrimary: '#24292e',
-        textSecondary: '#586069',
-        primary: '#0366d6',
-        white: '#fafafa'
-    },
-    fontSizes: {
-        body: 14,
-        subHeading: 16,
-    },
-    fonts: {
-        main: 'System'
-    },
-    fontWeigths: {
-        normal: '400',
-        bold: '700',
-    }
+  appBar: {
+    primary: '#24292e',
+    textPrimary: '#fff'
+  },
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+    white: '#fafafa'
+  },
+  fontSizes: {
+    body: 14,
+    subHeading: 16,
+  },
+  fonts: {
+    main: 'System'
+  },
+  fontWeigths: {
+    normal: '400',
+    bold: '700',
+  }
 }
 
 export default theme;
